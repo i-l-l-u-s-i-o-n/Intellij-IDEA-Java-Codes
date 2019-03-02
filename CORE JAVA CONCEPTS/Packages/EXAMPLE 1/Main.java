f0b7b16c7461a16c84c1e359bc9069ade8865456
Shivam1097 <shivam.dev1097@gmail.com>
2019-03-02 22:47:16 +0530

    Done.

---
+++ CORE JAVA CONCEPTS/Packages/EXAMPLE 1/Main.java
@@ -0,0 +1,14 @@
+
+// Created by Shivam Shukla.
+
+import myPackage1.*;    // Imorting Special class from myPackage1.
+
+public class Main{
+	public static void main(String[] args){
+		
+		Special s=new Special();
+		
+		s.msg();
+	}
+}
+
