78f63dbaf27ab624fb25f3cc6ba901104b64b0d3
Shivam1097 <shivam.dev1097@gmail.com>
2019-04-04 20:57:09 +0530

    Initial Commit

---
+++ Lambda Expression and Functional Interfaces/New_Features_in_JAVA_8.java
@@ -0,0 +1,26 @@
+package com.shivam;
+
+public class New_Features_in_JAVA_8 {
+
+    public static void main(String[] args) {
+
+        /*
+
+        Java 8 new features are ->
+
+        1. Lambda expression.
+        2. Functional Interfaces
+        3. Default Methods
+        ~ Predefined functional interfaces.
+                4. Predicates
+                5. Functions
+                   Supplier
+                   Consumer
+        6. Double colon operator
+            (Constructor and method references.)
+        7. Stream API
+        8. Date and Time API(Joda API)
+
+         */
+    }
+}
