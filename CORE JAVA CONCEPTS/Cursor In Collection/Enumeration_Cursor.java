aacb631d04f1066ae2595a394a377c16735145d2
Shivam1097 <shivam.dev1097@gmail.com>
2019-09-16 19:57:22 +0530

    Initial Commit

--- CORE JAVA CONCEPTS/Cursor In Collection/Enumeration_Cursor.java
+++ CORE JAVA CONCEPTS/Cursor In Collection/Enumeration_Cursor.java
@@ -22,9 +22,12 @@ class Main {
     public static void main(String[] args) {
 
 
-        // 1.) Enumeration ->
+// 1.) Enumeration ->
                             // We can get the Enumeration object by calling .elements() on the Vector object.
                             // Only applicable to Vector!!!!!!!!!!
+                            // It came in 1.0v so it is a Legacy Element.
+                            // It is not a universal cursor as t can be only used with Vector!!
+                            // It can only provide READ access and we can't remove elements!!!!
 
         Vector<Integer> integers = new Vector<>();
 
