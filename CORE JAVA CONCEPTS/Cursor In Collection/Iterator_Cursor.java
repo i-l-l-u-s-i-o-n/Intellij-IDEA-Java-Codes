06ff840da5582e1437121f55c9e4e056a864a5e1
Shivam1097 <shivam.dev1097@gmail.com>
2019-09-16 20:06:42 +0530

    Initial Commit

---
+++ CORE JAVA CONCEPTS/Cursor In Collection/Iterator_Cursor.java
@@ -0,0 +1,64 @@
+package collection;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * created by Shivam on 16-09-2019.
+ */
+
+
+// If we want to get objects one by one from the collection then we should go for Cursor.
+
+// There are 3 types of Cursor ->
+
+// 1.) Enumeration
+// 2.) Iterator
+// 3.) ListIterator
+
+
+public class Iterator_Cursor {
+
+    public static void main(String[] args) {
+
+        // 2.) Iterator->
+                // To overcome the problems of the Enumeration, Iterator comes into picture!!
+                // It is applicable to any Collection object and hence it is UNIVERSAL CURSOR.
+                // We can perform both read and remove operation!!!!!!!!
+                // The Iterator object can be created using .iterator() on collection object.
+
+        ArrayList<Integer> integers = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            integers.add(i);
+        }
+
+        Iterator<Integer> iterator =integers.iterator();
+
+        // Following are the methods in Iterator interface->
+        while (iterator.hasNext()){
+
+            Integer i = iterator.next();
+
+            if (i%2!=0)
+                System.out.println(i);
+
+            else
+                iterator.remove();
+        }
+
+        System.out.println(integers);
+
+    }
+}
+
+/*
+
+1
+3
+5
+7
+9
+[1, 3, 5, 7, 9]
+
+ */
