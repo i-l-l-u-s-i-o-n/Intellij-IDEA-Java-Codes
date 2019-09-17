0cad61f8c04d7b89ed7d57cfb69ca0075601c113
Shivam1097 <shivam.dev1097@gmail.com>
2019-09-17 21:20:14 +0530

    Initial Commit

---
+++ CORE JAVA CONCEPTS/Collection Framework/_WeakHashMap_.java
@@ -0,0 +1,82 @@
+package collection;
+
+import java.util.HashMap;
+import java.util.WeakHashMap;
+
+/**
+ * created by Shivam on 17-09-2019.
+ */
+public class _WeakHashMap_ {
+
+
+    public static void main(String[] args) throws Exception {
+
+
+        // In case of HashMap, even though a object doesn't have the reference but is currently used by HashMap as a Key or Value,
+        // then it will be not available for GARBAGE COLLECTION.
+
+        // Example ->
+        HashMap hashMap = new HashMap();
+
+        //Creating a new Temp object
+        Temp t1 =new Temp();
+
+        // Adding it to hashMap
+        hashMap.put(t1,"Temp");
+
+        // Now setting t1 to null.
+        t1=null;
+
+        // Now garbage collector will reach the temp object to destroy!!!!!!
+        // But HashMap won't allow GC to destroy temp object even though it is set to null because temp object is used as a key in HashMap!!!!!!!!!!
+        // So HashMap DOMINATES over GC and prevents GC to destroy objects that are used in HashMap.
+
+        Thread.sleep(2000);
+
+        // Calling garbage collector BUT it will not destroy Tem object!
+        System.gc();
+
+        // We can check this as finalise() will not be called!
+        // So no Output.
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+        // BUT in case of WeakHashMap, the GC dominates over the WeakHashMap hence named as WeakHashMap.
+
+        WeakHashMap weakHashMap = new WeakHashMap();
+
+        t1=new Temp();
+
+        weakHashMap.put(t1,"Temp");
+
+        // Now setting t1 to null;
+        t1=null;
+
+        // Now calling GC to destroy temp object and it will be destroyed.
+        Thread.sleep(2000);
+
+        System.gc();                 // It will call the finalize()
+
+
+        // OUTPUT -> Garbage Collector called!!
+
+
+
+    }
+}
+
+
+
+class Temp{
+
+    @Override
+    public String toString() {
+        return "Temp";
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.println("Garbage Collector called!!");
+    }
+}
