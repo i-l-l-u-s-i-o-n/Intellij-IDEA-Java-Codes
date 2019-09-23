250fe27d3eaf19813582db9e967874b26ea606c0
Shivam1097 <shivam.dev1097@gmail.com>
2019-09-24 01:22:55 +0530

    Initial Commit

---
+++ CORE JAVA CONCEPTS/ENUM(Enumeration)/_08_enum_VS_Enum_VS_Enumeration.java
@@ -0,0 +1,20 @@
+package enumeration;
+
+/**
+ * created by Shivam on 24-09-2019.
+ */
+public class _08_enum_VS_Enum_VS_Enumeration {
+
+    public static void main(String[] args) {
+
+        // enum -> It is a keyword which can be used to define a group of constants under a single name!
+        //         enum is the Direct child of Enum class.
+
+        // Enum -> It is an abstract class present in java.lang package, and is child class of Object and
+        //          implements Serializable and Comparable.
+
+        // Enumeration -> It is an interface, and used to get the objects one by one from Collection.
+        //                It is a CURSOR for Collection objects.
+
+    }
+}
